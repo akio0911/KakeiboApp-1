f5f0481a8b2002159ac5eaedcd0f7be770d820a5
kyohei_i <kyoheishogun.1997@i.softbank.jp>
2021-09-05 12:26:59 +0900

    ModelLocatorを実装

---
+++ KakeiboApp/New Group-Model/ModelLocator.swift
@@ -0,0 +1,15 @@
+//
+//  ModelLocator.swift
+//  KakeiboApp
+//
+//  Created by 今村京平 on 2021/09/05.
+//
+
+import Foundation
+
+// modelを共有するための構造体
+struct ModelLocator {
+    static let shared = ModelLocator()
+    let model = KakeiboModel()
+    private init() {}
+}
