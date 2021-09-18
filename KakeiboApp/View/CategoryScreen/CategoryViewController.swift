f28f0ac80312f643acac084c149044bc2885bc7b
kyohei_i <kyoheishogun.1997@i.softbank.jp>
2021-09-18 13:13:16 +0900

    CategoryScreenを追加

---
+++ KakeiboApp/View/CategoryScreen/CategoryViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CategoryViewController.swift
+//  KakeiboApp
+//
+//  Created by 今村京平 on 2021/09/18.
+//
+
+import UIKit
+
+class CategoryViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
