a8c829859f66d95206228f258e8c99d455292d39
kyohei_i <kyoheishogun.1997@i.softbank.jp>
2021-09-19 21:31:45 +0900

    CategoryViewControllerを実装

--- KakeiboApp/View/CategoryScreen/CategoryViewController.swift
+++ KakeiboApp/View/CategoryScreen/CategoryViewController.swift
@@ -6,9 +6,64 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class CategoryViewController: UIViewController {
+class CategoryViewController: UIViewController, UITableViewDelegate {
+
+    @IBOutlet private weak var categoryTableView: UITableView!
+
+    private let viewModel: CategoryViewModelType
+    private let disposeBag = DisposeBag()
+    private let categoryTableViewDataSource = CategoryTableViewDataSource()
+    private var headerDataArray: [HeaderDateCategoryData] = []
+
+    init(viewModel: CategoryViewModelType) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+
+    private func setupBinding() {
+        viewModel.outputs.cellDateDataObservable
+            .bind(to: categoryTableView.rx.items(dataSource: categoryTableViewDataSource))
+            .disposed(by: disposeBag)
+
+        viewModel.outputs.headerDateDataObservable
+            .subscribe(onNext: { [weak self] data in
+                guard let self = self else { return }
+                self.headerDataArray = data
+            })
+            .disposed(by: disposeBag)
+    }
+
+    private func setupTableView() {
+        categoryTableView.register(
+            CalendarTableViewCell.nib,
+            forCellReuseIdentifier: CalendarTableViewCell.identifier
+        )
+        categoryTableView.register(
+            CalendarTableViewHeaderFooterView.nib,
+            forHeaderFooterViewReuseIdentifier: CalendarTableViewHeaderFooterView.identifier
+        )
+        categoryTableView.sectionHeaderHeight = 17
+        categoryTableView.rx.setDelegate(self).disposed(by: disposeBag)
+    }
+
+    // MARK: - UITableViewDelegate
+    // ヘッダーのタイトルを設定
+//    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+//        guard let headerView = tableView.dequeueReusableHeaderFooterView(
+//                withIdentifier: CategoryTableViewHeaderView.identifier)
+//                as? CategoryTableViewHeaderView else { return nil }
+//        headerView.configure(data: headerDataArray[section])
+//        return headerView
+//    }
 }
