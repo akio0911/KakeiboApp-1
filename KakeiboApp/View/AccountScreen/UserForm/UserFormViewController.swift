27a6ac6ef0ab1dc624fd951100df12f092668627
kyohei_i <kyoheishogun.1997@i.softbank.jp>
2021-10-24 15:40:22 +0900

    UserFormViewのレイアウトを実装

---
+++ KakeiboApp/View/AccountScreen/UserForm/UserFormViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UserFormViewController.swift
+//  KakeiboApp
+//
+//  Created by 今村京平 on 2021/10/24.
+//
+
+import UIKit
+
+class UserFormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
